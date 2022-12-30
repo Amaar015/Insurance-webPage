39f41c5f1bc7e4ef3a5fb24e17029f93acab5768
Amaar Hassnain <93473921+Amaar015@users.noreply.github.com>
2022-12-30 09:49:59 +0500

    Our partner part is added to insurance webPage

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import Feature from './Component/Feature/Feature';
 import Home from './Component/Home/Home';
 import Services from './Component/Services/Services';
-
+import Partner from './Component/Partner/Partner';
 function App() {
   return (
       <>
       <Home/>
       <Services/>
       <Feature/>
+      <Partner/>
       </>
      
   );
--- src/Component/Partner/Partner.js
+++ src/Component/Partner/Partner.js
@@ -1,14 +1,29 @@
 import React from 'react'
 import './partner.css';
+import part1 from './../image/nike.png';
+import part2 from './../image/nb.png';
+import part3 from './../image/adidas.png';
+
 const Partner = () => {
   return (
-    <div className="partner">
+    <div className="partners">
 
        <p className='title'>Services</p>
         <h2 className="heading">Find the financial services you need</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
         Saepe similique laudantium illum error tenetur ab sunt iure, adipisci molestiae,
         </p>
+
+        <div className="images">
+           <img src={part1} alt="" />
+           <img src={part2} alt="" />
+           <img src={part3} alt="" />
+           <img src={part1} alt="" />
+           <img src={part2} alt="" />
+           <img src={part3} alt="" />
+        </div>
+
+        <button className='btn btn-partner'>Learn More</button>
     </div>
   )
 }
