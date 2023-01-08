44b2a5e03403970f72f568636d5dc978a288dcca
Amaar Hassnain <93473921+Amaar015@users.noreply.github.com>
2023-01-08 14:50:20 +0500

    insurance update

--- src/Component/Save/Save.js
+++ src/Component/Save/Save.js
@@ -8,6 +8,17 @@ const Save = () => {
           <p className='title t-safety'>Safetey</p>
           <span>Your cards scores always</span> 
           <span className='top'>Safe and Secure</span>
+          <div className="cards">
+            <h4>Credit Cards Secure</h4>
+            <p>Lorem ipsum dolor sit amet consectetur 
+              adipisicing elit. Sint, velit!</p>
+          </div>
+          <div className="cards">
+            <h4>Credit Cards Secure</h4>
+            <p>Lorem ipsum dolor sit amet consectetur 
+              adipisicing elit. Sint, velit!</p>
+          </div>
+          <button className='btn'>More Details</button>
           </div>
           <div className="s-right ">
                  <img src={image} alt="" />
